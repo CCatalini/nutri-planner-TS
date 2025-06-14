28fce27e66c98c81b41b6b3b5ce1412c9152ad10
ccatalini <camila.catalini@ing.austral.edu.ar>
2025-06-13 23:16:24 -0300

    🛟notifications versions

---
+++ app/src/main/java/com/austral/nutri_planner_ts/notification/NotificationConstants.kt
@@ -0,0 +1,12 @@
+package com.austral.nutri_planner_ts.notification
+
+
+/**
+ * Shared constants for the NutriPlanner notification system.
+ */
+const val notificationChannelID = "nutriplanner_notification_channel"
+
+const val EXTRA_NOTIFICATION_TYPE = "notification_type"
+
+const val DAILY_MEAL_REMINDER = "daily_meal_reminder"
+const val HYDRATION_REMINDER = "hydration_reminder" 
